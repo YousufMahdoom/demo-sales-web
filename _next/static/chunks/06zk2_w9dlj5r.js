ec762a8daf39332c8a199597e3eb369ced6f3c7d
YousufMahdoom <yousufmahdoom@gmail.com>
2026-08-06 04:14:34 +0000

    Deploying to gh-pages from @ YousufMahdoom/demo-sales-web@6f28db63117079b2d7c17b2d26373f4dd235cad0 🚀

---
+++ _next/static/chunks/06zk2_w9dlj5r.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,45657,o=>{o.v(t=>Promise.all(["static/chunks/2h8oxu7nm12m4.js"].map(t=>o.l(t))).then(()=>t(82010)))}]);
